e65a6056891b68d622f1e957f01ec31ef2218280
Jason Fang <jasonfangmagic@gmail.com>
2021-08-10 17:40:12 -0400

    Created Video Demo

--- Computer Vision/Advanced Computer Vision.py
+++ Computer Vision/Advanced Computer Vision.py
@@ -2,13 +2,13 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture('D:\Computer Vision Files\Videos\Matt_Ryan.AVI')
+cap = cv2.VideoCapture('D:\Computer Vision Files\Videos\Football_Tutorial.mp4')
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(
                static_image_mode=False,
                max_num_hands=2,
-               min_detection_confidence=0.5,
+               min_detection_confidence=0.8,
                min_tracking_confidence=0.8)
 mpDraw = mp.solutions.drawing_utils
 
@@ -29,7 +29,7 @@ while True:
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 print(id, cx, cy)
                 # if id == 4:
-                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                #cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
@@ -37,9 +37,9 @@ while True:
     fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-                (255, 0, 255), 3)
+    #put text
+    #cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                #(255, 0, 255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-    #1
