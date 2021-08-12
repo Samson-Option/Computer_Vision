6cfd8b7c34fa5176909b2438f1d4140711404e02
Jason Fang <jasonfangmagic@gmail.com>
2021-08-12 15:35:56 -0400

    Fit a CNN model

--- Computer Vision/Self_Driving_Training.py
+++ Computer Vision/Self_Driving_Training.py
@@ -1,5 +1,6 @@
 
 from utlis import *
+from sklearn.model_selection import train_test_split
 #First Step
 # import the data
 
@@ -9,9 +10,32 @@ data = importDataInfo(path)
 
 #second step
 #balance the data and create data visualization
+balanceData(data,display=False)
 
 
-from sklearn.model_selection import train_test_split
 xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steerings, test_size=0.2,random_state=10)
 print('Total Training Images: ',len(xTrain))
 print('Total Validation Images: ',len(xVal))
+
+#created CNN Model
+
+
+model = createModel()
+model.summary()
+
+#fit the model
+history = model.fit(batchGen(xTrain, yTrain, 100, 1),
+                                  steps_per_epoch=300,
+                                  epochs=10,
+                                  validation_data=batchGen(xVal, yVal, 100, 0),
+                                  validation_steps=200)
+
+#save the model
+model.save('model.h5')
+print('Model Saved')
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['Training', 'Validation'])
+plt.title('Loss')
+plt.xlabel('Epoch')
+plt.show()
--- Computer Vision/utlis.py
+++ Computer Vision/utlis.py
@@ -1,10 +1,14 @@
 #Step 1 import the data
-
+import cv2
 import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+import random
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
+
 
 #first step
 # import data
@@ -70,3 +74,75 @@ def loadData(path, data):
 imagesPath, steerings = loadData(path,data)
 
 
+def augmentImage(imgPath,steering):
+    img = mpimg.imread(imgPath)
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
+    img = pan.augment_image(img)
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1, 1.2))
+        img = zoom.augment_image(img)
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.2, 1.2))
+        img = brightness.augment_image(img)
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+    steering = -steering
+    return img, steering
+
+
+def preProcess(img):
+    img = img[60:135,:,:]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+def batchGen(imagesPath, steeringList, batchSize, trainFlag):
+    while True:
+        imgBatch = []
+
+    steeringBatch = []
+
+    for i in range(batchSize):
+        index = random.randint(0, len(imagesPath) - 1)
+    if trainFlag:
+        img, steering = augmentImage(imagesPath[index], steeringList[index])
+    else:
+        img = mpimg.imread(imagesPath[index])
+    steering = steeringList[index]
+    img = preProcess(img)
+    imgBatch.append(img)
+    steeringBatch.append(steering)
+    yield (np.asarray(imgBatch), np.asarray(steeringBatch))
+
+print('Setting UP')
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
+
+
+def createModel():
+    model = Sequential()
+
+    model.add(Convolution2D(24, (5, 5), (2, 2), input_shape=(66, 200, 3), activation='elu'))
+    model.add(Convolution2D(36, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(48, (5, 5), (2, 2), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
+    model.add(Convolution2D(64, (3, 3), activation='elu'))
+
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dense(1))
+
+    model.compile(Adam(lr=0.0001), loss='mse')
+    return model
+model = createModel()
+model.summary()
